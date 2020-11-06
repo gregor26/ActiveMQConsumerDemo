57f3737e48e47eedae04dc77119a910c458f241f
Gregor Felzer <gregor.felzer@anton-paar.com>
2020-11-06 11:33:55 +0100

    Project added to the git

---
+++ ActiveMQConsumerDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using Apache.NMS;
+using System;
+
+namespace ActiveMQConsumerDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("ActiveMQ Consumer Demo");
+
+            string topic = "TextQueue";
+
+            string brokerUri = $"activemq:tcp://192.168.250.198:61616";  // Default port
+            NMSConnectionFactory factory = new NMSConnectionFactory(brokerUri);
+
+            using (IConnection connection = factory.CreateConnection("artemis", "simetraehcapa"))
+            {
+                connection.Start();
+                using (ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
+                using (IDestination dest = session.GetQueue(topic))
+                using (IMessageConsumer consumer = session.CreateConsumer(dest))
+                {
+                    IMessage msg = consumer.Receive();
+                    if (msg is ITextMessage)
+                    {
+                        ITextMessage txtMsg = msg as ITextMessage;
+                        string body = txtMsg.Text;
+
+                        Console.WriteLine($"Received message: {txtMsg.Text}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unexpected message type: " + msg.GetType().Name);
+                    }
+                }
+            }
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Press any key to finish.");
+            Console.ReadKey(true);
+        }
+    }
+}
